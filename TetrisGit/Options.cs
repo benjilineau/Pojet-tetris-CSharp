49688810853ae80c9fdbd0a3f3f5eceb19214ee7
CReype <costa.reype@ynov.com>
2022-01-23 19:38:13 +0100

    Option modify

--- TetrisGit/Options.cs
+++ TetrisGit/Options.cs
@@ -22,7 +22,7 @@ namespace Tetris
             InitializeComponent();
             trackBar1.Value=5;
             wmp.settings.volume=5;
-            label2.Text = trackBar1.Value.ToString();
+            label3.Text = "Volume = " + trackBar1.Value.ToString();
 
         }
         
@@ -36,13 +36,45 @@ namespace Tetris
         private void trackBar1_Scroll(object sender, EventArgs e)
         { 
             wmp.settings.volume = trackBar1.Value;
-            label2.Text = trackBar1.Value.ToString();
+            label3.Text = "Volume = " + trackBar1.Value.ToString();
 
         }
 
         private void Options_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+            //Options obj1 = new Tuto();
+            //obj1.Show();
+            this.Hide();
         }
     }
 }
